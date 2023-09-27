611b4faef97d6607bf0cf1f2eee250e6188434ef
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-09-26 23:08:21 -0300

    feat: integrations test in docker

---
+++ src/test/java/br/com/athenas/desafio/integrationstests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,26 @@
+package br.com.athenas.desafio.integrationstests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.athenas.desafio.configs.TestConfigs;
+import br.com.athenas.desafio.integrationstests.testcontainers.AbstractIntegrationTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void shouldDisplaySwaggerUiPage() {
+        var content = given()
+                .basePath("/athenas/swagger-ui/index.html")
+                .port(TestConfigs.SERVER_PORT)
+                .when().get().then()
+                .statusCode(200)
+                .extract().body().asString();
+        assertTrue(content.contains("Swagger UI"));
+    }
+
+}
